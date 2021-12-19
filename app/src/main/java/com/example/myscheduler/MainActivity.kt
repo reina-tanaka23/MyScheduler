c4a89015223932c515d59bd7224a04fe1c322b80
reina.tanaka <reina.tanaka0223@gmail.com>
2021-12-19 21:58:45 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myscheduler/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.myscheduler
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.myscheduler.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            navController.navigate(R.id.action_to_scheduleEditFragment)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
+    fun setFabVisible(visibility: Int) {
+        binding.fab.visibility = visibility
+    }
+}
---
+++ app/src/main/java/com/example/myscheduler/ScheduleEditFragment.kt
@@ -0,0 +1,146 @@
+package com.example.myscheduler
+
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.os.Bundle
+import android.text.format.DateFormat.format
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.myscheduler.databinding.FragmentScheduleEditBinding
+import com.google.android.material.snackbar.Snackbar
+import io.realm.Realm
+import io.realm.kotlin.createObject
+import io.realm.kotlin.where
+import java.lang.IllegalArgumentException
+//import java.text.DateFormat
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+import android.text.format.DateFormat
+
+class ScheduleEditFragment : Fragment() {
+
+    private var _binding: FragmentScheduleEditBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var realm: Realm
+
+    private val args: ScheduleEditFragmentArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        realm = Realm.getDefaultInstance()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentScheduleEditBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (args.scheduleId != -1L) {
+            val schedule = realm.where<Schedule>().equalTo("id", args.scheduleId).findFirst()
+            binding.dateEdit.setText(DateFormat.format("yyyy/MM/dd", schedule?.date))
+            binding.timeEdit.setText(DateFormat.format("HH:mm", schedule?.date))
+            binding.titleEdit.setText(schedule?.title)
+            binding.detailEdit.setText(schedule?.detail)
+            binding.delete.visibility = View.VISIBLE
+        } else {
+            binding.delete.visibility = View.INVISIBLE
+        }
+        (activity as? MainActivity)?.setFabVisible(View.INVISIBLE)
+        binding.save.setOnClickListener { saveSchedule(it) }
+        binding.delete.setOnClickListener {
+            val dialog = ConfirmDialog(
+                "削除しますか？",
+                "削除", { deleteSchedule(it) },
+                "キャンセル", {
+                    Snackbar.make(it, "キャンセルしました", Snackbar.LENGTH_SHORT)
+                        .show()
+                })
+            dialog.show(parentFragmentManager, "delete_dialog")
+        }
+        binding.dateButton.setOnClickListener {
+            DateDialog{ date ->
+                binding.dateEdit.setText(date)
+            }.show(parentFragmentManager, "date_dialog")
+        }
+        binding.timeButton.setOnClickListener {
+            TimeDialog{ time ->
+                binding.timeEdit.setText(time)
+            }.show(parentFragmentManager, "time_dialog")
+        }
+    }
+
+    private fun saveSchedule(view: View) {
+        when (args.scheduleId) {
+            -1L -> {
+                realm.executeTransaction { db: Realm ->
+                    val maxId = db.where<Schedule>().max("id")
+                    val nextId = (maxId?.toLong() ?: 0L) + 1L
+                    val schedule = db.createObject<Schedule>(nextId)
+                    val date = "${binding.dateEdit.text} ${binding.timeEdit.text}".toDate()
+                    if (date != null) schedule.date = date
+                    schedule.title = binding.titleEdit.text.toString()
+                    schedule.detail = binding.detailEdit.text.toString()
+                }
+                Snackbar.make(view, "追加しました", Snackbar.LENGTH_SHORT)
+                    .setAction("戻る") { findNavController().popBackStack() }
+                    .setActionTextColor(Color.YELLOW)
+                    .show()
+            }
+             else -> {
+                 realm.executeTransaction { db: Realm ->
+                     val schedule = db.where<Schedule>().equalTo("id", args.scheduleId).findFirst()
+                     val date = ("${binding.dateEdit.text} " + "${binding.timeEdit.text}").toDate()
+                     if (date != null) schedule?.date = date
+                     schedule?.title = binding.titleEdit.text.toString()
+                     schedule?.detail = binding.detailEdit.text.toString()
+                 }
+                 Snackbar.make(view, "修正しました", Snackbar.LENGTH_SHORT)
+                     .setAction("戻る") { findNavController().popBackStack() }
+                     .setActionTextColor(Color.YELLOW)
+                     .show()
+             }
+        }
+    }
+
+    private fun deleteSchedule(view: View) {
+        realm.executeTransaction { db: Realm ->
+            db.where<Schedule>().equalTo("id", args.scheduleId)?.findFirst()?.deleteFromRealm()
+        }
+        Snackbar.make(view, "削除しました", Snackbar.LENGTH_SHORT)
+            .setActionTextColor(Color.YELLOW)
+            .show()
+
+        findNavController().popBackStack()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
+    }
+
+    private fun String.toDate(pattern: String = "yyyy/MM/dd HH:mm"): Date? {
+        return try {
+            SimpleDateFormat(pattern).parse(this)
+        } catch (e: IllegalArgumentException) {
+            return null
+        } catch (e: ParseException) {
+            return null
+        }
+    }
+}
